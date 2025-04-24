0c236371484f26ddb977e78ae50cb7754904c6eb
Eduardo F <145373957+ic-ed@users.noreply.github.com>
2025-04-24 10:38:03 -0500

    Create Utils.java

---
+++ Utils.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Utils {
+    private static final Scanner scanner = new Scanner(System.in)
+    public static String solicitarCampo(String mensaje) {
+        System.out.print(mensaje);
+        String input = scanner.nextLine().trim();
+
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException("El campo no puede estar vacío.");
+        }
+
+        return input;
+    }
+}
