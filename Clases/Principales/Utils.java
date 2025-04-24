481392dbd6b5d70167675d4a273560b49dfe6046
Eduardo F <145373957+ic-ed@users.noreply.github.com>
2025-04-24 13:27:24 -0500

    Update Utils.java
    Saque la necesidad de regex para la funcion de solicitarCampo

--- Clases/Principales/Utils.java
+++ Clases/Principales/Utils.java
@@ -12,18 +12,14 @@ public class Utils {
      * @return El input válido del usuario.
      * @throws IllegalArgumentException si el input no es válido.
      */
-    public static String solicitarCampo(String mensaje, String regex) {
+    public static String solicitarCampo(String mensaje) {
         System.out.print(mensaje);
         String input = scanner.nextLine().trim();
 
         if (input.isEmpty()) {
             throw new IllegalArgumentException("❌ El campo no puede estar vacío.");
         }
-
-        if (!input.matches(regex)) {
-            throw new IllegalArgumentException("❌ El formato ingresado no es válido. Intenta de nuevo.");
-        }
-
+        
         return input;
     }
 
