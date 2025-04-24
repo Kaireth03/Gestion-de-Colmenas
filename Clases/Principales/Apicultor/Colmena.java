684dc55401e42361eb1803aa08f9cea0bffc0128
Eduardo F <145373957+ic-ed@users.noreply.github.com>
2025-04-24 13:14:42 -0500

    Update Colmena.java
    Creado to-dos en colmena.java para ayudar y organizar

--- Clases/Principales/Apicultor/Colmena.java
+++ Clases/Principales/Apicultor/Colmena.java
@@ -2,6 +2,25 @@ package Clases.Principales;
 
 import java.util.ArrayList;
 
+public class Colmena {
+    // TODO: Declarar los atributos:
+    // - String id
+    // - String ubicacion
+    // - String tipo
+    // - String estadoDeSalud
+    // - byte cantidadAbejas
+    // - float produccionMiel
+
+    // TODO: Crear un constructor que reciba todos los atributos como parámetros
+
+    // TODO: Crear métodos getters para cada atributo
+
+    // TODO: Crear métodos setters para estadoDeSalud, cantidadAbejas y produccionMiel
+
+    // TODO: Sobrescribir el método toString para mostrar toda la información de la colmena
+}
+
+
 public class GestorApicultores {
     private ArrayList<Apicultor> apicultores;
 
@@ -19,3 +38,5 @@ public class GestorApicultores {
         }
     }
 }
+
+
