44f13f52b86ea424116f15f4f52688e940adb8c6
Eduardo F <145373957+ic-ed@users.noreply.github.com>
2025-04-24 16:39:35 -0500

    Update SistemaApicola.java
    Agregado los mismo todo de main

--- Clases/Principales/SistemaApicola.java
+++ Clases/Principales/SistemaApicola.java
@@ -2,22 +2,47 @@ public class SistemaApicola {
     private static DatosApicola datosApicola = new DatosApicola();
     
     public static void registrarApicultor() {
-        // Implementación aquí
+        // TODO: Pedir datos del apicultor (nombre, edad, experiencia, etc.)
+        // TODO: Validar los datos
+        // TODO: Crear objeto Apicultor
+        // TODO: Agregarlo a la lista correspondiente
+        // TODO: Confirmar el registro
     }
 
     public static void asignarAbejaReina() {
-        // Implementación aquí
+        // TODO: Mostrar lista de colmenas sin reina
+        // TODO: Permitir elegir colmena
+        // TODO: Pedir datos de la abeja reina (nombre, edad, etc.)
+        // TODO: Validar datos
+        // TODO: Asignar la abeja reina a la colmena seleccionada
+        // TODO: Confirmar asignación
     }
 
     public static void mostrarInformacion() {
-        // Implementación aquí
+        // TODO: Mostrar submenú con tipos de información a mostrar
+        // TODO: Dependiendo de la opción, mostrar:
+        //   - Colmenas registradas
+        //   - Apicultores
+        //   - Abejas reinas
+        //   - Historial de inspecciones
+        // TODO: Permitir volver al menú principal
     }
 
     public static void asignarApicultorAColmena() {
-        // Implementación aquí
+        // TODO: Mostrar lista de colmenas
+        // TODO: Mostrar lista de apicultores
+        // TODO: Permitir seleccionar ambos
+        // TODO: Validar que el apicultor no esté ya asignado
+        // TODO: Asignar apicultor a la colmena
+        // TODO: Confirmar asignación
     }
 
     public static void editarInformacion() {
-        // Implementación aquí
+        // TODO: Mostrar submenú: ¿qué desea editar? (colmena, apicultor, abeja reina, inspección)
+        // TODO: Según elección, mostrar lista de elementos
+        // TODO: Permitir seleccionar uno
+        // TODO: Pedir nuevos datos
+        // TODO: Validar y aplicar cambios
+        // TODO: Confirmar edición
     }
 }
